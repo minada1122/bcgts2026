aac143430fe3b934a67b482a935092fffe3ef26e
bcg <minaday41@gmail.com>
2025-05-20 22:13:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+var typed= new Typed(".text",{
+	strings: ["Bandaranayake College" , "Technology Society" , "Official Website"],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop:true
+});
+
+// ------------------------------------------------slider------------------------------------------------------------------
+
+const slides = document.querySelectorAll(".slide");
+let current = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) {
+      slide.classList.add("active");
+    }
+  });
+}
+
+function nextSlide() {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}
+
+setInterval(nextSlide, 3000); // Rotate every 3 seconds
+
+
+// -----------------------------------------------------slider end------------------------------------------------------------
+
+// -----------------------------------------------------Countdown Timer------------------------------------------------------
+
+
+// Mehemai oyā countdown ekata daanna time eka
+const targetDateString = "june 10, 2025 00:00:00";
+
+document.getElementById("target-display").textContent = targetDateString;
+
+const targetDate = new Date(targetDateString).getTime();
+
+const countdown = setInterval(() => {
+  const now = new Date().getTime();
+  const timeLeft = targetDate - now;
+
+  if (timeLeft <= 0) {
+    clearInterval(countdown);
+    document.getElementById("message").textContent = "🎉 Today is the Day";      //🎉 Countdown Complete!
+    updateCountdown(0, 0, 0, 0);
+    return;
+  }
+
+
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  updateCountdown(days, hours, minutes, seconds);
+}, 1000);
+
+function updateCountdown(days, hours, minutes, seconds) {
+  document.getElementById("days").textContent = formatNumber(days);
+  document.getElementById("hours").textContent = formatNumber(hours);
+  document.getElementById("minutes").textContent = formatNumber(minutes);
+  document.getElementById("seconds").textContent = formatNumber(seconds);
+}
+
+function formatNumber(num) {
+  return num < 10 ? "0" + num : num;
+}
+
+
+// -----------------------------------------------------Countdown Timer------------------------------------------------------
+
+
